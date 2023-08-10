a0ec6873079df7ae848f8d47a5292c2a1969c891
TimiMaliki <malikitimi@gmail.com>
2023-08-10 20:33:25 +0100

    about us page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import {
   Box,
+  Button,
   Grid,
   GridItem,
   Heading,
@@ -8,19 +9,40 @@ import {
 } from "@chakra-ui/react";
 import React from "react";
 import phone from "../assets/Imagies/phone.png";
+import rich from "../assets/Imagies/rich.jpg"
 
 const About = () => {
   return (
-    <SimpleGrid  minChildWidth="250px" p="90px" marginTop="2rem" alignItems="center">
-      <Box h="280px">
+    <SimpleGrid
+      minChildWidth="300px"
+      p="10px"
+      marginTop="2rem"
+      alignItems="center"
+      marginBottom="2rem"
+    >
+      <Box h="430px" alignItems="center" textAlign="center">
         <Text
           as="p"
-          fontSize={{base:"4xl", md:"6xl" , lg:"7xl"}}
+          fontSize={{ base: "4xl", md: "6xl", lg: "7xl" }}
           fontWeight="extrabold"
           color="blackAlpha.900"
         >
+
           Sell Gift Cards & Cryptocurrencies for Instant Cash.
         </Text>
+        <img src={rich} width="100px" height="100px" />
+        <Text
+          as="p"
+          fontSize={{ base: "xl", md: "2xl", lg: "3xl" }}
+          color="blackAlpha.900"
+        >
+          <p>
+            Great Exchange provides effortless means of trading all Giftcards
+            and Cryptocurrencies
+          </p>
+        </Text>
+         <button className="btn">Start Trading</button>
+       
       </Box>
 
       <Box h="200px">
@@ -31,4 +53,3 @@ const About = () => {
 };
 
 export default About;
-
